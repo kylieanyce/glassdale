f5ebb4cf0cbbd59d0dc03e8200f7eb4005d99c79
kylieanyce <kylieanyce@gmail.com>
2021-01-22 20:52:34 -0600

    added JS folders and files, and began working on criminal section

---
+++ scripts/criminals/CriminalList.js
@@ -0,0 +1,10 @@
+import { getCriminals, useCriminals } from './criminals/CriminalProvider.js'
+
+export const CriminalList = () => {
+    getCriminals().then(
+        /*
+            Now that you have the data, what
+            component should be rendered?
+        */
+    )
+}
