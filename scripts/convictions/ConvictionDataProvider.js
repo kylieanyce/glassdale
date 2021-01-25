aa8620f2667e380e69ef723a7954bc39c50489b2
kylieanyce <kylieanyce@gmail.com>
2021-01-25 14:56:55 -0600

    added conviction select drop down

---
+++ scripts/convictions/ConvictionDataProvider.js
@@ -0,0 +1,16 @@
+let convictions = []
+
+export const useConvictions = () => {
+    return convictions.slice()
+}
+
+export const getConvictions = () => {
+    return fetch("https://criminals.glassdale.us/crimes")
+        .then(response => response.json())
+        .then(
+            parsedConvictions => {
+                console.table(parsedConvictions)
+                convictions = parsedConvictions
+            }
+        )
+}
