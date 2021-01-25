adcd75a9c06af27460ba9fe4297b2cca718d466e
kylieanyce <kylieanyce@gmail.com>
2021-01-25 10:15:54 -0600

    rendered criminal list to dom

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,9 +1,22 @@
-import { getCriminals, useCriminals } from './criminals/CriminalProvider.js'
 
-const criminalContainer = document.querySelector(".criminalContainer")
+import { getCriminals, useCriminals } from './CriminalDataProvider.js'
+import { Criminals } from "./Criminal.js";
+
+const criminalContainer = document.querySelector(".criminalsContainer")
 
 export const CriminalList = () => {
     getCriminals()
-        .
-    )
+        .then(() => {
+            const criminalArray = useCriminals()
+            let criminalsHTMLrep = ""
+            for (const criminal of criminalArray) {
+                criminalsHTMLrep += Criminals(criminal)
+            }
+            criminalContainer.innerHTML = `
+                <h3>Glassdale Criminals</h3>
+                <section class="criminalList">
+                ${criminalsHTMLrep}
+                </section>`
+            }
+        )
 }
