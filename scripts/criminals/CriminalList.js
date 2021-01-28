d8d8ec3139967c771e78a92eb2cff7cf39c19edf
kylieanyce <kylieanyce@gmail.com>
2021-01-28 09:36:47 -0600

    added notes.json and now switching to new branch to do work there

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -56,17 +56,17 @@ eventHub.addEventListener("crimeChosen", event => {
 })
 
 //puts criminal name in officer select drop down----------------------------
-eventHub.addEventListener("officerSelect", event => {
-    // How can you access the officer name that was selected by the user?
-    const officerName = event.???
+// eventHub.addEventListener("officerSelect", event => {
+//     // How can you access the officer name that was selected by the user?
+//     const officerName = event.???
 
-    // How can you get the criminals that were arrested by that officer?
-    const criminals = useCriminals()
-    criminals.???(
-        criminalObject => {
-            if (criminalObject.??? === officerName) {
-                return true
-            }
-        }
-    )
-})
+//     // How can you get the criminals that were arrested by that officer?
+//     const criminals = useCriminals()
+//     criminals.???(
+//         criminalObject => {
+//             if (criminalObject.??? === officerName) {
+//                 return true
+//             }
+//         }
+//     )
+// })
