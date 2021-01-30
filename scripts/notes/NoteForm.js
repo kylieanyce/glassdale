60f28c6201a5505bc355921308b4355c293870ee
kylieanyce <kylieanyce@gmail.com>
2021-01-30 17:30:35 -0600

    finished noteform.js so it posts new notes to API

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,19 +1,22 @@
+import { saveNote } from "./NoteDataProvider.js";
+
 const contentTarget = document.querySelector(".noteFormContainer")
+const eventHub = document.querySelector(".container")
 
 const render = () => {
     contentTarget.innerHTML = `
 
-                <label for="note-date">Date: </label>
-                <input type="date" id="note-date">
-
                 <label for="note-suspect">Suspect: </label>
                 <input type="text" id="note-suspect">
 
-                <label for="note-author">Author: </label>
-                <input type="text" id="note-author">
-
                 <label for="note-text">Note: </label>
                 <input type="text" id="note-text">
+
+                <label for="note-date">Date: </label>
+                <input type="date" id="note-date">
+
+                <label for="note-author">Author: </label>
+                <input type="text" id="note-author">
 
                 <button id="saveNote">Save Note</button>
         
@@ -24,18 +27,21 @@ export const NoteForm = () => {
     render()
 }
 
-// eventHub.addEventListener("click", clickEvent => {
-//     if (clickEvent.target.id === "saveNote") {
-
-//         const newNote = {
-//             date: .value
-//         }
-
-//         // Change API state and application state
-//         saveNote(newNote)
-//     }
-// })
-
-// const NoteForm = () => {
-//     // rest of the code here
-// }
+eventHub.addEventListener("click", clickEvent => {
+    console.log("event")
+    if (clickEvent.target.id === "saveNote") {
+        clickEvent.preventDefault()
+        const suspect = document.getElementById("note-suspect").value
+        const note = document.getElementById("note-text").value
+        const date = document.getElementById("note-date").value
+        const author = document.getElementById("note-author").value
+        
+        const newNote = {
+            "note": note,
+            "suspect": suspect,
+            "date": date,
+            "author": author
+        }
+        saveNote(newNote)
+    }
+})
