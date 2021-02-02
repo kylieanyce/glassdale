05b4ed6ff2b5e0dd8f926c19aed5e5ca1163aa84
kylieanyce <kylieanyce@gmail.com>
2021-02-01 21:43:45 -0600

    worked on associateList

--- scripts/associates/AssociateList.js
+++ scripts/associates/AssociateList.js
@@ -1,4 +1,16 @@
+import { getCriminals } from "../criminals/CriminalDataProvider"
+
+
 const eventHub = document.querySelector(".container")
+
+
+export const AssociateList = () => {
+    getCriminals()
+        .then(() => {
+            const criminalArray = useCriminals()
+            }
+        )
+}
 
 eventHub.addEventListener("associatesClicked", event => {
     debugger
