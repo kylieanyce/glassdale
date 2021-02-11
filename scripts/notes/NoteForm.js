4d3030e4f5ea591cdf65df2336fe3fa1164299bf
kylieanyce <kylieanyce@gmail.com>
2021-02-11 12:21:48 -0600

    working on saving to API with criminal ID

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,6 +1,5 @@
 import { getCriminals, useCriminals } from "../criminals/CriminalDataProvider.js";
 import { saveNote } from "./NoteDataProvider.js";
-import { noteSelect } from "./NoteFormSelect.js";
 
 const contentTarget = document.querySelector(".noteFormContainer")
 const eventHub = document.querySelector(".container")
@@ -21,7 +20,7 @@ const render = (criminalsArray) => {
                 <select class="formBoxes">
                     <option value="0">Please select a criminal</option>
                     ${criminalsArray.map(criminal => {
-                        return `<option value="${criminal.id}">${criminal.name}</option>`
+                        return `<option id="note-suspect" value="${criminal.id}">${criminal.name}</option>`
                         })
                     }
                 </select>
@@ -44,17 +43,16 @@ const render = (criminalsArray) => {
 
 
 eventHub.addEventListener("click", clickEvent => {
-    // console.log("event")
     if (clickEvent.target.id === "saveNote") {
         clickEvent.preventDefault()
         const suspect = document.getElementById("note-suspect").value
-        const note = document.getElementById("note-text").value
+        const text = document.getElementById("note-text").value
         const date = document.getElementById("note-date").value
         const author = document.getElementById("note-author").value
         
         const newNote = {
             "suspect": suspect,
-            "note": note,
+            "text": text,
             "date": date,
             "author": author
         }
--- scripts/notes/NoteFormSelect.js
+++
@@ -1,7 +0,0 @@
-export const noteSelect = (criminal) => {
-    return  `
-            <select class="formBoxes">
-                <option value="${criminal.id}">${criminal.name}</option>
-            </select>
-        `
-        }
